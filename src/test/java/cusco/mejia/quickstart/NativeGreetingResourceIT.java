e0a9b38c5432f385b0941f6885c8e5343af69019
Fernando Cusco <bcuscom@est.ups.edu.ec>
2022-05-26 19:48:07 -0500

    project

---
+++ src/test/java/cusco/mejia/quickstart/NativeGreetingResourceIT.java
@@ -0,0 +1,9 @@
+package cusco.mejia.quickstart;
+
+import io.quarkus.test.junit.SubstrateTest;
+
+@SubstrateTest
+public class NativeGreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
